acb2022ff2bbd342b4b91f685f3b87045fa73072
Deepak Pathak <deepak.pathak2206@gmail.com>
2015-10-18 17:26:31 -0700

    atrou debugged

--- fcn_mil/src/save_png_colormap_pascal.m
+++ fcn_mil/src/save_png_colormap_pascal.m
@@ -2,9 +2,9 @@
 
 clear all; close all; clc;
 
-dirAddress = '/mnt/a/pathak/fcn_mil_cache/visualized_output/seg12test_best/';
+dirAddress = '/mnt/a/pathak/fcn_mil_cache/visualized_output/seg12test_size_untuned/';
 inputImages = dir(fullfile(dirAddress,'results/VOC2012/Segmentation/comp6_test_cls/*.png'));
-
+fprintf('Dir: %s\n',dirAddress);
 cmap = VOClabelcolormap(256);
 for i=1:length(inputImages)
     if mod(i,100)==0
